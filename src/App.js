d05be6e9466c8a6aaadead859d42167f4e72d804
audreymendzef <audreymendzef@gmail.com>
2022-09-23 14:39:04 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import SmallHeader from './Headers/SmallHeader.tsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SmallHeader>
+          Migraine trackers
+        </SmallHeader>
+       
       </header>
     </div>
   );
---
+++ src/Headers/SmallHeader.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./SmallHeader.css";
+
+const SmallHeader = ({ children }) => (
+  <h4 className="SmallHeader">{children}</h4>
+);
+export default SmallHeader;

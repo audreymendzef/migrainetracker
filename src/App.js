8541b94e357202124e93d3300119bd8579dd1ea7
audreymendzef <audreymendzef@gmail.com>
2022-09-26 15:09:59 -0400

    refactor: abstract home out of app js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,8 @@
-import PsychologyIcon from "@mui/icons-material/Psychology";
-import Button from "@mui/material/Button";
-import MainHeader from "Headers/MainHeader.js";
-import "./App.css";
+import Home from "Pages/Home.js";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <PsychologyIcon sx={{ fontSize: "5em", color: "#D3D3D3" }} />
-        <MainHeader>Migraine Monitor</MainHeader>
-        <p className="MainIntro">
-          Track your migraines in a quick and easy way to discover early warning
-          signs, triggers, and remedies that work best for you.
-        </p>
-        <div className="main-buttons">
-          <Button
-            variant="contained"
-            sx={{
-              background: "#D3D3D3",
-              color: "#282c34",
-              marginRight: "15px",
-              fontWeight: "800",
-              fontSize: "0.85em",
-              fontFamily: "Courier New",
-              width: "125px",
-              "&:hover": { backgroundColor: "#778899" },
-            }}
-          >
-            Logs
-          </Button>
-          <Button
-            variant="contained"
-            sx={{
-              background: "#D3D3D3",
-              color: "#282c34",
-              marginLeft: "15px",
-              fontWeight: "bold",
-              fontSize: "0.85em",
-              fontFamily: "Courier New",
-              width: "125px",
-              "&:hover": { backgroundColor: "#778899" },
-            }}
-          >
-            + Track
-          </Button>
-        </div>
-      </header>
-    </div>
-  );
+  return <Home />;
+  // return <Survey />
 }
 
 export default App;

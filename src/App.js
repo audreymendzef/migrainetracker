a431ca0686cb4a50a308cd2d70c48e012d4a512f
audreymendzef <audreymendzef@gmail.com>
2022-09-27 16:35:20 -0400

    feat: add survey form information

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Home from "Pages/Home";
+import Survey from "Pages/Survey";
 
 function App() {
-  return <Home />;
-  // return <Survey />
+  // return <Home />;
+  return <Survey />;
 }
 
 export default App;

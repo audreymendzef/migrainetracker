060678176ce1910c5885e4b88467dce9cb10bdaa
audreymendzef <audreymendzef@gmail.com>
2022-10-03 16:53:39 -0400

    feat: add mui form elements for survey page 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Home from "Pages/Home";
 import Survey from "Pages/Survey";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   // return <Home />;
-  return <Survey />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="survey" element={<Survey />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
